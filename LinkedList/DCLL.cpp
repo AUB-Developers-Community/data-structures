fd660784555c5d3552dc8d60f5ab4ad0a5ec42b1
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-10 19:43:39 +0600

    Create DCLL.cpp

---
+++ LinkedList/DCLL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Structure of Doubly Circular Linked List
+struct Node {
+  int data;
+  struct Node* next;
+  struct Node* prev;
+};
+
+int main() {
+  // implementation
+  
+  return 0;
+}
